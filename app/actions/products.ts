bd8a76303f63bd642346c5b9048cdd6b27559c69
Prajot Baloliya <83273160+s1xine@users.noreply.github.com>
2026-02-15 12:50:03 +0530

    debugging vercel fetch

--- app/actions/products.ts
+++ app/actions/products.ts
@@ -9,15 +9,40 @@ export type Product = {
   image: string;
 };
 
+// export const getProducts = cache(async (): Promise<Product[]> => {
+//   try {
+//     const res = await fetch("https://fakestoreapi.com/products", {
+//       next: { revalidate: 300 },
+//     });
+
+//     if (!res.ok) return [];
+//     return res.json();
+//   } catch {
+//     return [];
+//   }
+// });
+
 export const getProducts = cache(async (): Promise<Product[]> => {
   try {
+    console.log("Fetching products...");
+
     const res = await fetch("https://fakestoreapi.com/products", {
       next: { revalidate: 300 },
     });
 
-    if (!res.ok) return [];
-    return res.json();
-  } catch {
+    console.log("Status:", res.status);
+
+    if (!res.ok) {
+      console.error("API failed");
+      return [];
+    }
+
+    const data = await res.json();
+    console.log("Products:", data.length);
+
+    return data;
+  } catch (err) {
+    console.error("Fetch crashed:", err);
     return [];
   }
 });
