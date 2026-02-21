77c10e6c1d41186f158b5dcbaa7b900c698f6548
Prajot Baloliya <83273160+s1xine@users.noreply.github.com>
2026-02-21 19:16:12 +0530

    fix: same products on home page

--- components/products/home-products.tsx
+++ components/products/home-products.tsx
@@ -4,7 +4,10 @@ import { getHomepageProducts } from "@/lib/db/queries/products-queries";
 
 export default async function HomeProducts() {
   const { bestSellers, newArrivals } = await getHomepageProducts();
-  const products: Product[] = [...bestSellers, ...newArrivals];
+
+  const products: Product[] = Array.from(
+    new Map([...bestSellers, ...newArrivals].map((p) => [p.slug, p])).values(),
+  );
 
   return <ProductsGrid products={products} isHome={true} />;
 }
