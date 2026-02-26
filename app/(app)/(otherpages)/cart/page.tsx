d203a939fded26a3f3286e8a0aa55ad76d6de391
Prajot Baloliya <83273160+s1xine@users.noreply.github.com>
2026-02-26 18:12:54 +0530

    refactor: update layout structure to improve suspense handling

--- app/(app)/(otherpages)/cart/page.tsx
+++ app/(app)/(otherpages)/cart/page.tsx
@@ -1,13 +1,14 @@
 import PageHeader from "@/components/page-header";
 import CartPageClient from "@/components/cart/cart-page-client";
+import { Suspense } from "react";
 
 export default function CartPage() {
   return (
-    <>
+    <Suspense>
       <PageHeader title="Cart" />
       <section className="pb-32 pt-6 max-w-7xl mx-auto px-6">
         <CartPageClient />
       </section>
-    </>
+    </Suspense>
   );
 }
